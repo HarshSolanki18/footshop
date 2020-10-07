62763fcfcf0f5e7799de9a369354a704734b2ead
Harsh Solanki <hsolanki1884@gmail.com>
2020-10-07 14:45:24 +0530

    switch to es modules express updated node 12 to 14

--- backend/server.js
+++ backend/server.js
@@ -1,18 +1,22 @@
-const express=require('express');
-const app=express();
-const products=require('./data/products');
+import express from "express";
+import dotenv from "dotenv";
+import products from  "./data/products.js";
 
-app.get('/',(req,res)=>{
-    res.send('API is running')
-})
+dotenv.config();
+const app = express();
+app.get("/", (req, res) => {
+  res.send("API is running");
+});
 
-app.get('/api/products',(req,res)=>{
-    res.json(products);
-})
+app.get("/api/products", (req, res) => {
+  res.json(products);
+});
 
-app.get('/api/products/:id',(req,res)=>{
-    const product =products.find(p=>p._id===req.params.id)
-    res.json(product);
-})
+app.get("/api/products/:id", (req, res) => {
+  const product = products.find((p) => p._id === req.params.id);
+  res.json(product);
+});
 
-app.listen(5000,console.log('Server running on 5000'))
+const PORT=process.env.PORT||5000
+
+app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${process.env.PORT}`));
