4a51f898c0e15780aadee50f0d1423b462962c95
Harsh Solanki <hsolanki1884@gmail.com>
2020-10-17 11:22:05 +0530

    user registration backend

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -44,3 +44,35 @@ export const getUserProfile = asyncHandler(async (req, res) => {
       throw new Error('User not found')
     }
   })
+//creates a new user
+// POST /api/users
+// public
+
+export const registerUser = asyncHandler(async (req, res) => {
+  const { name,email, password } = req.body;
+
+  const userExits = await User.findOne({ email });
+
+  if(userExits){
+    res.status(400);
+    throw new Error('User already exists');
+  }
+  const user=await User.create({
+    name,
+    email,
+    password
+  })
+  if(user){
+    res.status(201)
+    res.json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+      token: generateToken(user._id),
+    });
+  }else{
+    res.status(400)
+    res.json('invalid user dqta')
+  }
+});
