cab60c33895837101ae4950edebccf5bdaf00a7c
Harsh Solanki <hsolanki1884@gmail.com>
2020-11-10 17:00:05 +0530

    user registration user profile and update dashboard

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,6 +8,8 @@ import CartScreen from './components/Screens/CartScreen';
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import ProductScreen from "./components/Screens/ProductScreen";
 import LoginScreen from "./components/Screens/LoginScreen";
+import RegisterScreen from "./components/Screens/RegisterScreen";
+import ProfileScreen from "./components/Screens/ProfileScreen";
 
 function App() {
   return (
@@ -19,6 +21,8 @@ function App() {
           <Route path="/product/:id" component={ProductScreen} />
           <Route path='/cart/:id?'component={CartScreen}/>
           <Route path='/login' component={LoginScreen}/>
+          <Route path='/register' component={RegisterScreen} />
+          <Route path='/profile' component={ProfileScreen}/>
         </Container>
       </main>
       <Footer />
--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,30 +1,38 @@
 import { createStore, combineReducers, applyMiddleware } from "redux";
 import thunk from "redux-thunk";
 import { composeWithDevTools } from "redux-devtools-extension";
-import { productDetailsReducer, productListReducer } from "./reducers/productReducer";
-import {cartReducer} from './reducers/cartReducer';
-import {userLoginReducer} from './reducers/userReducer';
+import {
+  productDetailsReducer,
+  productListReducer,
+} from "./reducers/productReducer";
+import { cartReducer } from "./reducers/cartReducer";
+import {
+  userDetailsReducer,
+  userLoginReducer,
+  userRegisterReducer,
+  userUpdateProfileReducer,
+} from "./reducers/userReducer";
 const reducer = combineReducers({
   productList: productListReducer,
-  productDetails:productDetailsReducer,
-  cart:cartReducer,
-  userLogin:userLoginReducer
+  productDetails: productDetailsReducer,
+  cart: cartReducer,
+  userLogin: userLoginReducer,
+  userRegister: userRegisterReducer,
+  userDetails: userDetailsReducer,
+  userUpdateProfile:userUpdateProfileReducer
 });
 
+const cartItemsFromStorage = localStorage.getItem("cartItems")
+  ? JSON.parse(localStorage.getItem("cartItems"))
+  : [];
 
-const cartItemsFromStorage = localStorage.getItem('cartItems')
-  ? JSON.parse(localStorage.getItem('cartItems'))
-  : []
-
-  const userInfoFromStorage = localStorage.getItem('userInfo')
-  ? JSON.parse(localStorage.getItem('userInfo'))
-  : null  
-
-
+const userInfoFromStorage = localStorage.getItem("userInfo")
+  ? JSON.parse(localStorage.getItem("userInfo"))
+  : null;
 
 const initialState = {
-  cart:{cartItems:cartItemsFromStorage},
-  userLogin:{userInfo:userInfoFromStorage}
+  cart: { cartItems: cartItemsFromStorage },
+  userLogin: { userInfo: userInfoFromStorage },
 };
 const middleware = [thunk];
 const store = createStore(
